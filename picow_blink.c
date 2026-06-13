1dfedbf105d41a953ed453c852578388666c5af3
Kosmokleaner <Kosmokleaner@users.noreply.github.com>
2026-06-13 00:06:01 -0700

    Update picow_blink.c

--- picow_blink.c
+++ picow_blink.c
@@ -2,14 +2,14 @@
 
 #include "pico/stdlib.h"
 #include "pico/cyw43_arch.h"
-
 #include "pico/bootrom.h"
 
 int main()
 {
     stdio_init_all();
 	// needed?
-    if (cyw43_arch_init()) {
+    if (cyw43_arch_init())
+	{
         printf("Wi-Fi init failed");
         return -1;
     }
@@ -19,6 +19,7 @@ int main()
 
     while (true)
     {
+		// toggle 0 / 1 state to blink LED on Pico 2 W
         cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, cnt % 2);
         sleep_ms(250);
 

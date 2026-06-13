08fbe4f19e3c16ddf973e0b03be2ecd10214bacb
Kosmokleaner <Kosmokleaner@users.noreply.github.com>
2026-06-13 00:04:57 -0700

    Update picow_blink.c

--- picow_blink.c
+++ picow_blink.c
@@ -1,14 +1,14 @@
+// see https://github.com/Kosmokleaner/RapidPicoC
+
 #include "pico/stdlib.h"
 #include "pico/cyw43_arch.h"
 
 #include "pico/bootrom.h"
-void reboot()
-{
-    reset_usb_boot(0, 0);
-}
 
-int main() {
+int main()
+{
     stdio_init_all();
+	// needed?
     if (cyw43_arch_init()) {
         printf("Wi-Fi init failed");
         return -1;
@@ -19,26 +19,19 @@ int main() {
 
     while (true)
     {
-        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1);
-        sleep_ms(250);
-        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 0);
+        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, cnt % 2);
         sleep_ms(250);
 
+		// connect with PUTTY for keyboard input
         int c = getchar_timeout_us(0);
-        if (c >= 0)
-        {
-            if (c == 27)
-                reboot();       // connect with PUTTY
-                // sudo dmesg | grep tty
-                //   e.g. /dev/ttyS0   115200
-        }                
+		// press ESC to reboot
+        if (c == 27)
+            reset_usb_boot(0, 0); // reboot
 
         cnt++;
         printf("cnt:%d\n", cnt);
         // sudo picotool load -x build/picow_blink.elf
         // next: wait for command sent to device
         // putty -serial /dev/ttyACM0 -sercfg 115200,8,n,1,N
-
     }
-    
 }
